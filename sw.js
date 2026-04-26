647ff8eb0740a84c37ec82f5f65d2c8e342d17af
dataoshark-spec <dataoshark@gmail.com>
2026-04-26 14:24:57 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'compound-v115';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      // 用 addAll 但容錯：某個 icon 不存在時不讓整個 install 失敗
+      Promise.all(
+        ASSETS.map(url =>
+          c.add(url).catch(err => console.warn('Failed to cache', url, err))
+        )
+      )
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
